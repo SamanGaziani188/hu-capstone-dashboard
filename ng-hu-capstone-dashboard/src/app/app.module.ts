39712c2e535d6e22dd43f3bd2ddb04d92b7e81e3
Saman Gaziani <sg02494@st.habib.edu.pk>
2018-10-28 22:50:06 -0700

    Login Page is complete

--- ng-hu-capstone-dashboard/src/app/app.module.ts
+++ ng-hu-capstone-dashboard/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { SignuppageComponent } from './signuppage/signuppage.component';
+
+const appRoutes: Routes = [
+    { path: 'login' , component: LoginComponent},
+    { path: '' , redirectTo: '/login' , pathMatch: 'full'},
+    { path: 'home' , component: MainpageComponent},
+    { path: 'signup' , component: SignuppageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainpageComponent,
+    SignuppageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
